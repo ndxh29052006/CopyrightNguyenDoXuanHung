add3ab79fdc468956ab72b17e69a08c1201cfc1d
Bùi Chí Thông <88557481+chithong2004@users.noreply.github.com>
2021-08-29 21:41:58 +0700

    Add files via upload

---
+++ modules/commands/cam.js
@@ -0,0 +1,44 @@
+module.exports.config = {
+  name: "cam",
+  version: "1.0.0",
+  hasPermssion: 0,
+  credits: "BuiChiThong",
+  description: "cắm sừng người bạn tag",
+  commandCategory: "general",
+  usages: "cam [tag người bạn cần cắm sừng]",
+  cooldowns: 5,
+  dependencies: {
+    "request":"",
+    "fs":""
+  }
+};
+
+module.exports.run = function({
+  api,
+  event,
+  args
+}) {
+  const fs = global.nodemodule["fs-extra"];
+  const request = global.nodemodule["request"];
+  var out = (msg) => api.sendMessage(msg, event.threadID, event.messageID);
+  if (!args.join(" ")) return out("Bạn chưa nhập tin nhắn");
+  else
+  return request('https://nekos.life/api/v2/img/classic', (err, response, body) => {
+    let picData = JSON.parse(body);
+    var mention = Object.keys(event.mentions)[0];
+    let getURL = picData.url;
+    let ext = getURL.substring(getURL.lastIndexOf(".") + 1);
+    let tag = event.mentions[mention].replace("@", "");
+    let callback = function() {
+      api.sendMessage({
+        body: tag + " Xin Chúc Mừng Bạn Đã Có Cặp Sừng Xinh Xinh !",
+        mentions: [{
+          tag: tag,
+          id: Object.keys(event.mentions)[0]
+        }],
+        attachment: fs.createReadStream(__dirname + `/cache/anime.${ext}`)
+      }, event.threadID, () => fs.unlinkSync(__dirname + `/cache/anime.${ext}`), event.messageID);
+    };
+    request(getURL).pipe(fs.createWriteStream(__dirname + `/cache/anime.${ext}`)).on("close", callback);
+  });
+}
