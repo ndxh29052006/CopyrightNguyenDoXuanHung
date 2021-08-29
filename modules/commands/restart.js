01ffbc9db035d6ef383f206e00f8141fae1a1c3f
Bùi Chí Thông <88557481+chithong2004@users.noreply.github.com>
2021-08-29 21:47:46 +0700

    Add files via upload

---
+++ modules/commands/restart.js
@@ -0,0 +1,10 @@
+module.exports.config = {
+	name: "restart",
+	version: "1.0.0",
+	hasPermssion: 2,
+	credits: "CThong",
+	description: "Khởi Động Lại Bot.",
+	commandCategory: "[ Source BCT ]",
+	cooldowns: 0
+        };
+module.exports.run = ({event, api}) =>api.sendMessage("Tụi Mày Chờ Tao Chút Thằng Admin Nó Khởi Động Tao Lại Tao Sẽ Quay Lại Trong Ít Phút <3",event.threadID, () =>process.exit(1))
